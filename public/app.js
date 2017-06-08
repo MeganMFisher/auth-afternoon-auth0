9fe83c74d4d1186a6697e088f821b6e1b9a15371
MeganMFisher <meganmichelle318@gmail.com>
2017-06-08 12:02:43 -0600

    heyyo

--- public/app.js
+++ public/app.js
@@ -22,5 +22,24 @@ angular.module('app', ['ui.router'])
             url: '/',
             templateUrl: './templates/friend.html'
         })
-
 })
+
+
+//   app.config(function($httpProvider) {
+//     $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+//     $httpProvider.interceptors.push('myHttpInterceptor');
+// })
+
+// // register the interceptor as a service
+//   app.factory('myHttpInterceptor', function() {
+//     return {
+//         'responseError': function(rejection) {
+//             if (rejection.status == 403) {
+//               document.location = '/';
+//                 return rejection;
+//             }
+//             return rejection;
+//         }
+//     };
+// })
+
--- public/homeCtrl.js
+++ public/homeCtrl.js
@@ -1,4 +1,5 @@
 angular.module('app').controller('homeCtrl', function($scope, githubService){
 
+$scope.friends = githubService.getFollowing()
 
 })
